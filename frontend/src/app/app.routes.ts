d06fc463225aab0b0b0a547201c3d937867bcdfb
Jesus Delgado <jesusdelgado20042001@gmail.com>
2025-09-17 20:52:59 -0300

    Subiendo proyecto a Jesus---Frontend

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'splash', 
+    pathMatch: 'full',
+  },
+  {
+    path: 'registro',
+    loadComponent: () => import('./registro/registro.page').then( m => m.RegistroPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'olvido',
+    loadComponent: () => import('./olvido/olvido.page').then( m => m.OlvidoPage)
+  },
+  {
+    path: 'splash',
+    loadComponent: () => import('./splash/splash.page').then( m => m.SplashPage)
+  },
+];
